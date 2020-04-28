3dde19da164cfaea348d5a4951e484f662200d16
CarsonLoveless <63983264+CarsonLoveless@users.noreply.github.com>
2020-04-27 22:31:16 -0700

    Add files via upload

--- MainMenuState.h
+++ MainMenuState.h
@@ -1,16 +1,33 @@
 #pragma once
 #include "GameState.h"
+#include "iostream"
+
+#define PLAY 0
+#define RULES 1
+#define EXIT 2
+
+using namespace sf;
+
 class MainMenuState :
 	public GameState
 {
 private:
-	sf::CircleShape shape;
-	
+	Font font;
+	Text title;
+	Text menu;
 public:
+	// formats the main menu
 	MainMenuState();
 	void onSwitch(std::vector<SwitchFlags>& flags);
-	void draw(sf::RenderWindow& window);
-	States update(std::vector<sf::Event>& events, std::vector<SwitchFlags>& o_flags);
+	
+	// draws the main menu
+	void draw(RenderWindow& window);
+
+	// draws the entire rules tab
+	void rules(RenderWindow& window);
+	States update(std::vector<Event>& events, std::vector<SwitchFlags>& o_flags);
+	RectangleShape selector;
+	int selectorpos;
 	~MainMenuState();
 };
 
