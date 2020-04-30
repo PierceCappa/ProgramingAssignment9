f9f5f4ae74eff14b1a897fa8accf94d37368bb2d
SomeMysteryMan <60997038+SomeMysteryMan@users.noreply.github.com>
2020-04-29 22:42:58 -0700

    Update PlayState.cpp

--- PlayState.cpp
+++ PlayState.cpp
@@ -16,30 +16,54 @@ int hori = 10;
 int verti = 10;
 States PlayState::update(vector<sf::Event>& events)
 {
-	auto pos = sprite.getPosition();
-	if (pos.x > 800)
-	{
-		hori = -10;
-	}
-	else if (pos.x < 0)
-	{
-		hori = 10;
-	}
+			sf::Event event;
+		std::vector<Event> events;
+		std::vector<SwitchFlags> switchFlags;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed || gameBoard.checkIfLost() == true || gameBoardAI.checkIfLost() == true)
+			{
+				window.close();
 
-	if (pos.y > 800)
-	{
-		verti = -10;
-	}
-	else if (pos.y < 0)
-	{
-		verti = 10;
-	}
-
-	sprite.move(hori, verti);
-	return States::NO_CHANGE;
+			}
+			else if (event.type == Event::KeyPressed)
+			{
+				if (Keyboard::isKeyPressed(Keyboard::Up) && gameBoardAI.getY() > 0)
+				{
+					gameBoardAI.moveY(-1);
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Down) && gameBoardAI.getY() < 9)
+				{
+					gameBoardAI.moveY(1);
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Left) && gameBoardAI.getX() > 0)
+				{
+					gameBoardAI.moveX(-1);
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Right) && gameBoardAI.getX() < 9)
+				{
+					gameBoardAI.moveX(1);
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::Enter))
+				{
+					gameBoardAI.checkHit(gameBoardAI.getX(), gameBoardAI.getY());
+					choiceAI(x, y);
+					gameBoard.checkHit(x, y);
+				}
+			}
+			else
+			{
+				events.push_back(event);
+			}
+		}
 }
 
 void PlayState::draw(sf::RenderWindow& window)
 {
-	window.draw(sprite);
+		
+		window.clear();
+		labels.game(window);
+		gameBoardAI.drawBoard(window);
+		gameBoard.drawBoard(window);
+		window.display();
 }
